a3e4a3927fdb44034eec1cc8b3fbdbfc68bcca3b
Allenice <994298628@qq.com>
2017-08-01 03:57:06 -0400

    update demo code

--- demo/src/demo.vue
+++ demo/src/demo.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="code">
-    <div ref="result">
-
-    </div>
+    <component :is="child"></component>
     <div class="code-src">
       <pre>
         <code ref="code" class="html">
@@ -16,77 +14,85 @@
 <style src="highlight.js/styles/github-gist.css"></style>
 
 <style>
-.code {
-  display: flex;
-  max-width: 1400px;
-  margin: 0 auto;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-}
-
-.code > div {
-  float: left;
-  width: 50%;
-}
-
-code {
-  text-align: left;
-  line-height: 1.5;
-  font-size: 12px;
-  border-radius: 5px;
-  background: #f4f4f4 !important;
-  font-family: 'Roboto Mono', monospace;
-}
-
-@media(max-width: 1000px) {
   .code {
-    display: block;
+    display: flex;
+    max-width: 1400px;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
   }
+
   .code > div {
-    width: 100%;
+    float: left;
+    width: 50%;
   }
-}
+
+  code {
+    text-align: left;
+    line-height: 1.5;
+    font-size: 12px;
+    border-radius: 5px;
+    background: #f4f4f4 !important;
+    font-family: 'Roboto Mono', monospace;
+  }
+
+  @media(max-width: 1000px) {
+    .code {
+      display: block;
+    }
+    .code > div {
+      width: 100%;
+    }
+  }
 </style>
 
 <script>
-import Vue from 'vue'
-import codeMap from './codeMap'
-import hlj from './highlight.js'
+  import Vue from 'vue'
+  import codeMap from './codeMap'
+  import hlj from './highlight.js'
 
-export default {
-  props: ['code', 'datas'],
+  export default {
+    props: ['code', 'datas'],
 
-  computed: {
-    codeString () {
-      return codeMap[this.code]
-    }
-  },
-
-  mounted () {
-    new Vue({
-      el: this.$refs.result,
-      name: 'CodeDemo',
-      render: Vue.compile(`<div>${this.codeString[0]}</div>`).render,
-      data: () => {
-        return {
-          datas: this.datas || []
+    data () {
+      return {
+        child: {
+          name: 'child',
+          template: '<div></div>'
         }
       }
-    })
+    },
 
-    let codeEl = this.$refs.code
-    if (codeEl) {
-      codeEl.textContent = this.codeString[1]
-        ? this.codeString[1].replace(/#{{(\w+)}}/g, (match, name) => {
-          return this.codeString[0]
-        })
-        : this.codeString[0]
+    computed: {
+      codeString () {
+        return codeMap[this.code]
+      }
+    },
 
-      hlj.highlightBlock(codeEl)
+    mounted () {
+      this.child = {
+        name: 'preview',
+        template: `<div>${this.codeString[0]}</div>`,
+        data: () => {
+          return {
+            datas: this.datas || []
+          }
+        }
+      }
+
+      let codeEl = this.$refs.code
+      if (codeEl) {
+        codeEl.textContent = this.codeString[1]
+          ? this.codeString[1].replace(/#{{(\w+)}}/g, (match, name) => {
+            return this.codeString[0]
+          })
+          : this.codeString[0]
+
+        hlj.highlightBlock(codeEl)
+      }
     }
   }
-}
 </script>
 
 
