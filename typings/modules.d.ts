ee2df25d88cd9e34761907a186c1c9ef251dce22
allenice <994298628@qq.com>
2018-07-08 07:30:04 +0800

    update typings

--- typings/modules.d.ts
+++ typings/modules.d.ts
@@ -9,6 +9,28 @@ declare module '*.vue' {
 }
 
 declare module '@/components/SvgIcon.vue' {
-    import VueSvgIcon from 'typings'
+    import Vue from 'vue'
+    import { PluginFunction } from 'vue'
+
+    class VueSvgIcon extends Vue {
+        static icons: { [key: string]: Icon }
+        static register: (data: { [key: string]: Icon }) => void
+        static install: PluginFunction<{
+            tagName?: string
+            classPrefix?: string
+            isStroke?: boolean
+            defaultWidth?: string
+            defaultHeight?: string
+        }>
+    }
+
+    export type Icon = {
+        width: number
+        height: number
+        viewBox: string
+        data: string
+        [key: string]: any
+    }
+
     export default VueSvgIcon
 }
