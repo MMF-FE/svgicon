410336776e6fc11b1401581b80571ede4d2cadbc
allenice <994298628@qq.com>
2023-11-25 15:17:17 +0800

    update deploy doc script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,10 @@ set -e
 # 生成静态文件
 yarn build:doc
 
-cp -R demo/v3 docs/.vuepress/dist
+cp -R demo/v3 docs/.vitepress/dist
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
