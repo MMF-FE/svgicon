1a64ba3ca7a98a3b947535b9207e263652b292df
allenice <994298628@qq.com>
2023-12-15 15:22:28 +0800

    vue-cli-plugin-svgicon no longer injects code

--- packages/vue-cli-plugin-svgicon/generator/index.js
+++ packages/vue-cli-plugin-svgicon/generator/index.js
@@ -8,19 +8,11 @@ module.exports = (api, options) => {
         '@yzfe/svgicon': devDeps['@yzfe/svgicon'],
     }
 
-    if (options.isVue3) {
-        deps['@yzfe/vue-svgicon'] = devDeps['@yzfe/vue-svgicon']
-        api.injectImports(api.entryFile, [
-            `import { VueSvgIconPlugin } from '@yzfe/vue-svgicon'`,
-            `import '@yzfe/svgicon/lib/svgicon.css'`,
-        ])
-    } else {
-        deps['@yzfe/vue2-svgicon'] = devDeps['@yzfe/vue2-svgicon']
-        api.injectImports(api.entryFile, [
-            `import { VueSvgIcon } from '@yzfe/vue2-svgicon'`,
-            `import '@yzfe/svgicon/lib/svgicon.css'`,
-        ])
-    }
+    deps['@yzfe/vue-svgicon'] = devDeps['@yzfe/vue-svgicon']
+    api.injectImports(api.entryFile, [
+        `import { VueSvgIconPlugin } from '@yzfe/vue-svgicon'`,
+        `import '@yzfe/svgicon/lib/svgicon.css'`,
+    ])
 
     api.extendPackage({
         dependencies: deps,
@@ -31,34 +23,5 @@ module.exports = (api, options) => {
 }
 
 module.exports.hooks = (api, options) => {
-    api.afterInvoke(() => {
-        const { EOL } = require('os')
-        const fs = require('fs')
-        const contentMain = fs.readFileSync(api.resolve(api.entryFile), {
-            encoding: 'utf-8',
-        })
-
-        const lines = contentMain.split(/\r?\n/g)
-
-        if (options.isVue3) {
-            let injectCode = `.use(VueSvgIconPlugin, { tagName: '${options.tagName}' })`
-            const renderIndex = lines.findIndex((line) =>
-                line.match(/createApp\(\w+\)/)
-            )
-            lines[renderIndex] = lines[renderIndex].replace(
-                /createApp\(\w+\)/g,
-                (match) => {
-                    return match + injectCode
-                }
-            )
-        } else {
-            let injectCode = `
-Vue.component('${options.tagName}', VueSvgIcon)
-`
-            const renderIndex = lines.findIndex((line) => line.match(/new Vue/))
-            lines[renderIndex - 1] += `${injectCode}`
-        }
-
-        fs.writeFileSync(api.entryFile, lines.join(EOL), { encoding: 'utf-8' })
-    })
+    api.afterInvoke(() => {})
 }
